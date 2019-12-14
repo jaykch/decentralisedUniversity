60c0d9fd0be3fa544515c1bc07d70d9e872f0cab
jaykch <jaykch@outlook.com>
2019-12-14 15:50:04 +1100

    Created smart contract

--- src/contracts/DecentralisedUniversity.sol
+++ src/contracts/DecentralisedUniversity.sol
@@ -1,9 +1,41 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
 contract DecentralisedUniversity {
     string public name;
+    uint public studentCount = 0;
+    mapping(uint => Student) public students;
+
+    struct Student {
+        uint id;
+        string name;
+        string studentId;
+        string[] courses;
+        address author;
+    }
+
+    event StudentCreated(
+        uint id,
+        string name,
+        string studentId,
+        string[] courses,
+        address author
+    );
 
     constructor() public {
         name = "Decentralised University";
+    }
+
+    function createStudent(string memory _name, string memory _studentId, string[] memory _courses) public {
+        // Require valid content
+        require(bytes(_name).length > 0);
+        require(bytes(_studentId).length > 0);
+        require(string[](_courses).length > 0);
+        // Increment the student count
+        studentCount++;
+        // Create Student
+        students[studentCount] = Student(studentCount, _name, _studentId, _courses, msg.sender);
+        // Trigger Event
+        emit StudentCreated(studentCount, _name, _studentId, _courses, msg.sender);
     }
 }
--- test/DecentralisedUniversity.js
+++ test/DecentralisedUniversity.js
@@ -4,7 +4,7 @@ require('chai')
     .use(require('chai-as-promised'))
     .should();
 
-contract('DecentralisedUniversity', (accounts) => {
+contract('DecentralisedUniversity', ([deployer, author, updater]) => {
     let decentralisedUniversity;
 
     before(async () => {
@@ -24,5 +24,33 @@ contract('DecentralisedUniversity', (accounts) => {
             assert.equal(name, "Decentralised University");
         })
 
+        describe('student', async () => {
+            let result, studentCount;
+
+            it('creates student', async () => {
+                result = await decentralisedUniversity.createStudent('Jay Kumar', "s3770282", ['testing 1', 'testing 2'], {from: author});
+                studentCount = await decentralisedUniversity.studentCount();
+
+                // SUCCESS
+                assert.equal(studentCount, 1);
+                const event = result.logs[0].args;
+                assert.equal(event.id.toNumber(), studentCount.toNumber(), 'id is correct');
+                assert.equal(event.name, 'Jay Kumar', 'name is correct');
+                assert.equal(event.studentId, 's3770282', 'name is correct');
+                assert.deepEqual(event.courses, ['testing 1', 'testing 2'], 'courses are correct');
+                assert.equal(event.author, author, 'author is correct');
+
+                // FAILURE: Post must have valid arguments
+                await decentralisedUniversity.createStudent('', {from: author}).should.be.rejected;
+            });
+
+            it('lists student', async () => {
+                //TODO: Pending
+            });
+
+            it('updates student', async () => {
+                //TODO: Pending
+            });
+        })
     })
 });
